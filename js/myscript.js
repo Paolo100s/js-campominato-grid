af77fc2127aa7412d64d8f54db1c21518cb81428
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-05-10 12:09:18 +0200

    add numbers

---
+++ js/myscript.js
@@ -0,0 +1,39 @@
+// Consegna
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// variabili globali
+const gridElement = document.getElementById("grid");
+const play = document.getElementById("play");
+let numbOfCell;
+
+// generatore griglia
+play.addEventListener('click',
+    function () {
+        let cells = 100;
+
+        // ciclo
+        for (i = 1; i <= cells; i++) {
+
+            // creazione elemento
+            const square = document.createElement('div');
+
+            // aggiunta classe
+            square.classList.add("square")
+
+            // aggiunta alla griglia
+            gridElement.append(square);
+
+            // aggiunta numero nelle celle
+            numbOfCell = i;
+            square.innerHTML = numbOfCell;
+
+           
+        }
+
+    }
+
+)
+
